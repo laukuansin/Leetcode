2269bf9bd88625666fa53ec5390f2340f76ba5b8
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-04-17 20:35:41 +0800

    Time: 90 ms (81.43%), Space: 38 MB (17.86%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode IncreasingBST(TreeNode root) {
+        if(root==null)
+        {
+            return null;
+        }
+        Stack<int> stack = new Stack<int>();
+        helper(root,stack);
+        TreeNode newTree=new TreeNode(0,null,null);
+        TreeNode trackTree = newTree;
+       
+        while(stack.Count()>0)
+        {
+            int val = stack.Pop();
+            TreeNode tmp = new TreeNode(val);
+            trackTree.right=tmp;
+            trackTree.left=null;
+            trackTree=trackTree.right;
+        }
+        return newTree.right;
+    }
+    
+    public void helper(TreeNode root, Stack<int> stack)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        helper(root.right,stack);
+        stack.Push(root.val);
+        helper(root.left,stack);
+        
+        return;
+    }
+}
