b379f5cd0e12fa21a4c0e77ee3fd9ef6a9d69e37
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-04-18 21:11:05 +0800

    Time: 102 ms (79.08%), Space: 42.5 MB (19.39%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    int count =0,ans=0;
+    
+    public int KthSmallest(TreeNode root, int k) {
+        helper(root,k);
+        return ans;
+    }
+    
+    public void helper(TreeNode root,int k)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        helper(root.left,k);
+        count++;
+        if(k==count)
+            ans= root.val;
+        helper(root.right,k);
+        
+        return;
+    }
+}
