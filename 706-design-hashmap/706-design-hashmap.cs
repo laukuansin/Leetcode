959e30a0bb1a8351142aea75d7254e20528aa373
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-04-22 21:15:32 +0800

    Time: 562 ms (19.11%), Space: 57.6 MB (29.84%) - LeetHub

---
+++ 706-design-hashmap/706-design-hashmap.cs
@@ -0,0 +1,43 @@
+public class MyHashMap {
+    int[] keysVals;
+    bool[] checks;
+    
+    public MyHashMap() {
+        keysVals = new int[1000001];
+        checks= new bool[1000001];
+    }
+    
+    public void Put(int key, int value) {
+        if(checks[key])
+        {
+            keysVals[key]=value;
+        }
+        else{
+            checks[key]=true;
+            keysVals[key]=value;
+        }
+    }
+    
+    public int Get(int key) {
+        if(checks[key])
+        {
+            return keysVals[key];
+        }
+        else{
+            return -1;
+        }
+    }
+    
+    public void Remove(int key) {
+        checks[key]=false;
+        
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.Put(key,value);
+ * int param_2 = obj.Get(key);
+ * obj.Remove(key);
+ */
