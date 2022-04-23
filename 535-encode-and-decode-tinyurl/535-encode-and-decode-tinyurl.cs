20b62a86b393b503ef2f6392976ae64f92b91ad9
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-04-23 22:01:57 +0800

    Time: 94 ms (62.20%), Space: 37.6 MB (70.08%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cs
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cs
@@ -1,13 +1,38 @@
 public class Codec {
-
+    Dictionary<string,string> hashMap;
+    int num = 916132831;
+    string str = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     // Encodes a URL to a shortened URL
     public string encode(string longUrl) {
-        return longUrl;
+        Random rdm = new Random();
+        hashMap = new Dictionary<string,string>();
+        int randomNum = rdm.Next(num);
+        string ans = Convertto64(randomNum);
+        while(hashMap.ContainsKey(ans))
+        {
+            randomNum = rdm.Next(num);
+            ans=Convertto64(randomNum);
+        }
+        hashMap.Add(ans,longUrl);
+        return ans;
     }
+    
+    
+    public string Convertto64(int num)
+    {
+        string ans="";
+        while(num!=0)
+        {
+            ans+=str[(num%62)];
+            num/=62;
+        }
+        return ans;
+    }
+
 
     // Decodes a shortened URL to its original URL.
     public string decode(string shortUrl) {
-        return shortUrl;
+        return hashMap[shortUrl];
     }
 }
 
