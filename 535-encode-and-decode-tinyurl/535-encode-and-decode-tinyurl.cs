17ccaaf37e37343f0c470005c4fbf4d674ac4d0a
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-04-23 20:39:22 +0800

    Time: 82 ms (76.38%), Space: 37.4 MB (96.85%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cs
@@ -0,0 +1,16 @@
+public class Codec {
+
+    // Encodes a URL to a shortened URL
+    public string encode(string longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public string decode(string shortUrl) {
+        return shortUrl;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
