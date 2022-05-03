601ea9b41606ceb7978753e3d0fabe04bf4205ca
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-05-03 22:32:27 +0800

    Time: 162 ms (34.31%), Space: 41.6 MB (73.53%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cs
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public int FindUnsortedSubarray(int[] nums) {
+        /*  selection sort method
         int left = nums.Length,right=0;
         
         for(int i=0;i<nums.Length-1;i++)
@@ -15,5 +16,25 @@ public class Solution {
         }
         
         return right-left<0?0:right-left+1;
+        */
+        
+         Stack <int> stack = new Stack <int> ();
+        int l = nums.Length, r = 0;
+        for (int i = 0; i < nums.Length; i++) {
+            while (stack.Count!=0&& nums[stack.Peek()] > nums[i])
+            {
+                l = Math.Min(l, stack.Pop());
+            }
+            stack.Push(i);
+        }
+        stack.Clear();
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            while (stack.Count!=0 && nums[stack.Peek()] < nums[i])
+            { 
+                r = Math.Max(r, stack.Pop());
+            }
+            stack.Push(i);
+        }
+        return r - l > 0 ? r - l + 1 : 0;
     }
 }
