062bd3e6e28ae1d1eb5f80612c270e52fe378c25
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-05-05 21:52:11 +0800

    Time: 241 ms (67.74%), Space: 51.1 MB (12.90%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cs
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cs
@@ -1,26 +1,42 @@
 public class Solution {
     public int MaxOperations(int[] nums, int k) {
-        int start = 0;
-        int end=nums.Length-1;
-        int ans =0;
-        
-        Array.Sort(nums);
-        
-        while(start<end)
+        Dictionary<int,int> hashMap = new Dictionary<int,int>();
+        int ans=0;
+        foreach(int num in nums)
         {
-            if(nums[start]+nums[end]==k)
+            if(hashMap.ContainsKey(num))
             {
-                start++;
-                end--;
-                ans++;
-            }
-            else if(nums[start]+nums[end]>k)
-            {
-                end--;
+                hashMap[num]+=1;
             }
             else{
-                start++;
+                hashMap.Add(num,1);
             }
+        }
+        
+        foreach(KeyValuePair<int, int> kvp in hashMap)
+        {
+            
+            if(kvp.Value==0)
+            {
+                continue;
+            }
+           // Console.WriteLine(kvp.Key+"");
+            int target = k-kvp.Key;
+            
+            if(hashMap.ContainsKey(target))
+            {
+                if(target==kvp.Key)
+                {
+                    ans+=kvp.Value/2;
+                    hashMap[target]=0;
+                }
+                else{
+                    ans+= Math.Min(kvp.Value,hashMap[target]);
+                    hashMap[kvp.Key]=0;
+                    hashMap[target]=0;
+                }
+            }
+            
         }
         
         return ans;
