aec248682e17588397386fde3116f5d272e37703
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-05-29 21:39:04 +0800

    Time: 13 ms (62.98%), Space: 47.6 MB (43.28%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,12 +1,29 @@
 class Solution {
     public int maxProduct(String[] words) {
         int max = 0;
+        int size = words.length;
+        int[] bitMasks = new int[size];
+        for(int i=0;i<size;i++)
+        {
+            int total =0;
+            for(char c :words[i].toCharArray())
+            {
+                int num=(c-'a');
+                int temp = 1;
+                temp<<=num;
+                total|=temp;
+
+            }
+            bitMasks[i]=total;
+        }
+        
         
         for(int i=0;i<words.length-1;i++)
         {
             for(int j=i+1;j<words.length;j++)
             {
-                if(check(words[i],words[j]))
+                int conc=bitMasks[i]&bitMasks[j];
+                if(conc==0)
                 {
                     int currLength = words[i].length()*words[j].length();
                     max=Math.max(currLength,max);
@@ -16,17 +33,4 @@ class Solution {
         return max;
     }
     
-    private boolean check(String a, String b)
-    {
-        char[] charArr = a.toCharArray();
-        
-        for(char c : charArr)
-        {
-            if(b.indexOf(c)!=-1)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
 }
