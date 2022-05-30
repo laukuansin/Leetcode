4acc9bb547c17dcca0835520e67e11910ca2646f
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-05-30 21:59:49 +0800

    Time: 2 ms (62.23%), Space: 42 MB (12.93%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.java
+++ 29-divide-two-integers/29-divide-two-integers.java
@@ -4,25 +4,21 @@ class Solution {
             return Integer.MAX_VALUE;
         }
         
-        boolean pos = false;
-        if((dividend<0&&divisor<0)||(dividend>0&&divisor>0))
+        boolean positive = (dividend>0&&divisor>0)||(dividend<0&&divisor<0);
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int result=0;
+        int shift =31;
+        while(a>=b)
         {
-            pos=true;
-        }
-         long   a = Math.abs((long)dividend);
-          long  b = Math.abs((long)divisor);
-       
-        
-        int shift = 31;
-        int res = 0;
-        while (a >= b) {
-            while (a < b << shift) {
+            while(a<b<<shift)
+            {
                 shift--;
             }
-            a -= b << shift;
-            res += 1 << shift;
+            a-=b<<shift;
+            result+=1<<shift;
         }
         
-        return pos ? res : -res;
+        return positive?result:-result;
     }
 }
