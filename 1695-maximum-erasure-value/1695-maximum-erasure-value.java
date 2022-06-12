db47268e64796c395f829d0f435cd4be19b3f1d4
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-06-12 21:09:09 +0800

    Time: 269 ms (5.04%), Space: 120.8 MB (34.65%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -0,0 +1,33 @@
+import java.util.*;  
+
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        Hashtable<Integer,Integer> hashtable = new Hashtable<Integer,Integer>();
+        
+        int max=0;
+        int track=0;
+        
+        for(int start=0,end=0;end<nums.length;end++)
+        {
+            track+=nums[end];
+            hashtable.put(nums[end],hashtable.getOrDefault(nums[end],0)+1);
+            if(hashtable.get(nums[end])<2)
+            {
+                max=Math.max(max,track);
+            }
+            else{
+                while(hashtable.get(nums[end])>1)
+                {
+                    hashtable.put(nums[start],hashtable.getOrDefault(nums[start],0)-1);
+                    track-=nums[start];
+                    start++;
+                    
+
+                }
+            }
+            
+        }
+         
+        return max;
+    }
+}
