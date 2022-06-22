f2ef1278a6b22ae5ed416287d518ab7752aaff3f
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-06-22 22:48:24 +0800

    Time: 2 ms (94.61%), Space: 43.8 MB (78.96%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
+        /*
         PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder()); 
         int ans=0;
         for(int i=0;i<nums.length;i++)
@@ -16,5 +17,53 @@ class Solution {
             }
         }
         return ans;
+        */
+        
+        return quickSelect(nums,0,nums.length-1,nums.length-k);
+    }
+    
+    private int quickSelect(int[] nums,int left,int right,int k)
+    {
+        if(left==right)
+        {
+            return nums[left];
+        }
+        
+        int pIndex = new Random().nextInt(right - left + 1) + left;
+        pIndex=partition(nums,left,right,pIndex);
+        
+        if(k==pIndex)
+        {
+            return nums[k];
+        }
+        else if(k<pIndex)
+        {
+            return quickSelect(nums,left,pIndex-1,k);
+        }
+        else{
+            return quickSelect(nums,pIndex+1,right,k);
+        }
+    }
+    
+    private int partition(int[] nums,int left, int right, int index)
+    {
+        int pivot=nums[index];
+        int temp = nums[right];
+        nums[index]=temp;
+        nums[right]=pivot;
+        index=left;
+        
+        for(int j=left;j<=right;j++)
+        {
+            if(nums[j]<=pivot)
+            {
+                int temp1 = nums[index];
+                nums[index]=nums[j];
+                nums[j]=temp1;
+                index++;
+            }
+            
+        }
+        return index-1;
     }
 }
