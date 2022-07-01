dbdfcd9e7a4bf33636b94c73ad897b944ea584af
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-07-01 21:53:41 +0800

    Time: 1 ms (54.07%), Space: 42.6 MB (23.87%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,8 +1,8 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         s=s.trim();
-        
-        String[] strArr = s.split(" ");
+        String[] strArr=s.split(" ");
         return strArr[strArr.length-1].length();
+        //return s.trim().split(" ")[s.trim().split(" ").length-1].length();
     }
 }
