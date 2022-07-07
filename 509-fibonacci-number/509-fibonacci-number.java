6dcd79047ce813c908724b51c1191c66c5ddafff
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-07-07 21:54:10 +0800

    Time: 0 ms (100.00%), Space: 39.1 MB (87.87%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,16 +1,16 @@
 class Solution {
 
     public int fib(int n) {
-       if(n==0)
-       {
-           return 0;
-       }
-        if(n==1)
+        if(n<2)
+            return n;
+        int[] fib =new int[n+1];
+        fib[0]=0;
+        fib[1]=1;
+        for(int i=2;i<=n;i++)
         {
-            return 1;
+            fib[i]=fib[i-1]+fib[i-2];
         }
-        
-        return fib(n-1)+fib(n-2);
+        return fib[n];
     }
     
 }
