b0f9ab72b02a832ada34454bb04ddc0436c03e3e
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-07-15 22:43:13 +0800

    Time: 4 ms (56.02%), Space: 47.2 MB (54.50%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,4 +1,5 @@
 class Solution {
+    int temp=0;
     public int maxAreaOfIsland(int[][] grid) {
         int max = 0;
         boolean[][] visited= new boolean[grid.length][grid[0].length];
@@ -11,25 +12,27 @@ class Solution {
                     visited[i][j]=true;
                     continue;
                 }
-                max=Math.max(max,helper(grid,i,j,visited));
+                helper(grid,i,j,visited); 
+                max=Math.max(max,temp);
+                temp=0;
             }
         }
         return max;
     }
     
-    public int helper(int[][] grid, int x, int y, boolean[][] visited)
+    public void helper(int[][] grid, int x, int y, boolean[][] visited)
     {
         if(x<0||y<0||x>=grid.length||y>=grid[0].length||visited[x][y]||grid[x][y]==0)
         {
-            return 0 ;
+            return;
         }
         
         visited[x][y]=true;
-        
-        return (1+ helper(grid,x+1,y,visited)+
-        helper(grid,x-1,y,visited)+
-        helper(grid,x,y+1,visited)+
-        helper(grid,x,y-1,visited));
+        temp++;
+        helper(grid,x+1,y,visited);
+        helper(grid,x-1,y,visited);
+        helper(grid,x,y+1,visited);
+        helper(grid,x,y-1,visited);
     }
 }
 
