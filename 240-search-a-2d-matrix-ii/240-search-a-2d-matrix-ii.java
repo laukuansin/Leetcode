e00f40a048c01234ec875609ef2e99f67a594b99
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-07-24 21:57:05 +0800

    Time: 5 ms (100.00%), Space: 48 MB (96.47%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,33 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        /*
-        boolean check = false;
-        int row=matrix.length;
-        int col=matrix[0].length;
-          
-        int start = 0;
-        int end=row-1;
-
-        while(start<=end)
-        {
-            int middle=(start+end)/2;
-            if(matrix[middle][col-1]==target)
-            {
-                return true;
-            }
-            else if(target>matrix[middle][col-1]){
-                start=middle+1;
-            }
-            else{
-                if(binarySearch(matrix,middle,target))
-                {
-                    return true;
-                }
-                end=middle-1;
-            }
-        }
-        return false;
-        */
+        
         int row=matrix.length, col=matrix[0].length;
         int start=0, end=col-1;
         while(start<row&&end>=0){
@@ -40,27 +13,5 @@ class Solution {
         }
         return false;
     }
-    public boolean binarySearch(int[][] matrix,int found,int target)
-    {
-        int row=matrix.length;
-        int col=matrix[0].length;
-        int start=0;
-        int end=col-1;
-        while(start<=end)
-        {
-            int middle=(start+end)/2;
-            if(matrix[found][middle]==target)
-            {
-                return true;
-            }
-            else if(matrix[found][middle]>target)
-            {
-                end=middle-1;
-            }
-            else{
-                start=middle+1;
-            }
-        }
-        return false;
-    }
+
 }
