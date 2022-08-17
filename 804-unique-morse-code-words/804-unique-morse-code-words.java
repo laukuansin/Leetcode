6ba8795c90d951380a949f748a4b7c46baf3c251
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-08-17 21:27:31 +0800

    Time: 2 ms (99.55%), Space: 40.5 MB (89.75%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -2,8 +2,9 @@ import java.util.Hashtable;
 
 class Solution {
     public int uniqueMorseRepresentations(String[] words) {
+        /*
         Hashtable<Character,String> ht = new Hashtable<>();
-        HashSet<String> set=new HashSet();  
+          
         ht.put('a',".-");ht.put('b',"-...");ht.put('c',"-.-.");
         ht.put('d',"-..");ht.put('e',".");ht.put('f',"..-.");
         ht.put('g',"--.");ht.put('h',"....");ht.put('i',"..");
@@ -13,6 +14,9 @@ class Solution {
         ht.put('s',"...");ht.put('t',"-");ht.put('u',"..-");
         ht.put('v',"...-");ht.put('w',".--");ht.put('x',"-..-");
         ht.put('y',"-.--");ht.put('z',"--..");
+        */
+        HashSet<String> set=new HashSet();
+        String[] strArr = new String[] {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         
         for(String str:words)
         {
@@ -20,13 +24,11 @@ class Solution {
             String tmp ="";
             for(char c:charArr)
             {
-                tmp+=ht.get(c);
+                tmp+=strArr[c-'a'];
             }
             set.add(tmp);
         }
         
-        
-        int ans=0;
         return set.size();
     }
 }
