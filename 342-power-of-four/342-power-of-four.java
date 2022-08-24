477eb9a05a83020b62a63d06bd0f3d0c662a394b
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-08-24 21:40:20 +0800

    Time: 2 ms (65.34%), Space: 41.6 MB (25.85%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+       ///return n>0 && (n & (n-1)) == 0 && (n-1)%3==0;
+    return (Math.log10(n) / Math.log10(4)) % 1 == 0;
+
+    }
+}
