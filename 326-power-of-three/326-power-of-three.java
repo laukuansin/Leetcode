6211fc2e6ec883f3a8c5bcc54455c1c045bbfabf
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-08-24 20:59:59 +0800

    Time: 14 ms (99.77%), Space: 41.7 MB (99.28%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0)
+            return false;
+        while(n>=3)
+        {
+            if(n%3!=0)
+            {
+                return false;
+            }
+            n/=3;
+        }
+        return n==1?true:n%3==0;
+    }
+}
