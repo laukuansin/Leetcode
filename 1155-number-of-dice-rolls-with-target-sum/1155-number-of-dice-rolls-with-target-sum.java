98b70d59de41817364b40725bec1f511b7b96775
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-10-02 22:52:29 +0800

    Time: 21 ms (78.05%), Space: 49.6 MB (15.10%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    int MOD = 1000000007;
+    int[][] dp;
+    
+    Solution()
+    {
+        dp = new int[31][1001];
+        for (int[] row: dp)
+            Arrays.fill(row,-1);
+    }
+    
+    public int numRollsToTarget(int n, int k, int target) {
+        if(target<0)
+            return 0;
+        if(n==0)
+        {
+            return target==0?1:0;
+        }
+        
+        if(dp[n][target]!=-1)
+        {
+            return dp[n][target];
+        }
+        int ans=0;
+        for(int i=1;i<=k;i++)
+        {
+            ans=(ans+numRollsToTarget(n-1,k,target-i))%MOD;
+        }
+        return dp[n][target]=ans%MOD;
+    }
+}
