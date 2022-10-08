baacddd3dcfaec165d9d598b72208a7a4ed03952
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-10-08 21:15:06 +0800

    Time: 160 ms (9.38%), Space: 42.2 MB (93.91%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -5,14 +5,12 @@ class Solution {
         int res = Integer.MAX_VALUE;
         for(int i=0;i<=size-3;i++)
         {
-            int firstNum = nums[i];
-            
             int start = i+1;
             int end = nums.length-1;
             
             while(start<end)
             {
-                int tmpTotal =firstNum+nums[start]+nums[end];
+                int tmpTotal =nums[i]+nums[start]+nums[end];
                 if(target == tmpTotal)
                 {
                     return target;
