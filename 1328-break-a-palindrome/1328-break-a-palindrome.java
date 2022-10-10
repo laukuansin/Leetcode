be390c260090987568969ebcd2bb456a0a90e82c
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-10-10 20:06:17 +0800

    Time: 0 ms (100.00%), Space: 42.1 MB (50.43%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -6,9 +6,7 @@ class Solution {
             return "";
         
         char[] charArr = palindrome.toCharArray();
-        if(size%2==0)
-        {
-            for(int i=0;i<size/2;i++)
+        for(int i=0;i<size/2;i++)
             {
                 if(charArr[i]!='a')
                 {
@@ -16,20 +14,8 @@ class Solution {
                     return new String(charArr);
                 }
             }
-            charArr[size-1]='b';
-            return new String(charArr);
-        }
-        else{
-             for(int i=0;i<size/2;i++)
-            {
-                if(charArr[i]!='a')
-                {
-                    charArr[i]='a';
-                    return new String(charArr);
-                }
-            }
-            charArr[size-1]='b';
-            return new String(charArr);
-        }
+        charArr[size-1]='b';
+        return new String(charArr);
+        
     }
 }
