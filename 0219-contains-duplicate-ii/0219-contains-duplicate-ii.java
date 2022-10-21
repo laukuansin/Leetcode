01916b05ca6b04529f0126a9a8155195e9f8a255
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-10-21 20:41:46 +0800

    Time: 134 ms (7.68%), Space: 99 MB (5.73%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,23 @@
+import java.util.Hashtable;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(nums.length==1) return false;
+        Hashtable<Integer,Integer> ht = new Hashtable<>();
+        int num = k>=nums.length?nums.length-1:k;
+        for(int i=0;i<=num;i++)
+        {
+            ht.put(nums[i],ht.getOrDefault(nums[i],0)+1);
+            if(ht.get(nums[i])>1)
+                return true;
+        }
+        for(int i=1;i<nums.length-k;i++)
+        {
+            ht.put(nums[i-1],ht.get(nums[i-1])-1);
+            ht.put(nums[i+k],ht.getOrDefault(nums[i+k],0)+1);
+            if(ht.get(nums[i+k])>1)
+                return true;
+        }
+        return false;
+        
+    }
+}
