74ff2a6266a37353ad9effb8def185f5d67c9002
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-10-29 20:48:38 +0800

    Time: 107 ms (70.78%), Space: 125.9 MB (26.94%) - LeetHub

---
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        if(plantTime.length==1&&growTime.length==1)
+        {
+            return plantTime[0]+growTime[0];
+        }
+        List<Pair<Integer,Integer>> list = new ArrayList<>();
+        
+        for(int i=0;i<plantTime.length;i++)
+        {
+            list.add(new Pair<Integer,Integer>(plantTime[i],growTime[i]));
+        }
+        Collections.sort(list, new Comparator<Pair<Integer,Integer>>() {
+          public int compare(Pair<Integer,Integer> p1, Pair<Integer,Integer> p2) {
+            if (p1.getValue() > p2.getValue()) return -1;
+            if (p1.getValue() < p2.getValue()) return 1;
+            return 0;
+          }});
+        int cur = 0;
+        int max = 0;
+        
+        for(Pair<Integer,Integer> pair:list)
+        {
+            //key: grow time
+            //value: plant time
+            cur+=pair.getKey();
+            max = Math.max(cur+pair.getValue(),max);
+            //System.out.println("V:"+entry.getValue()+",key:"+entry.getKey());
+        }
+        
+        return max;
+        
+    }
+}
