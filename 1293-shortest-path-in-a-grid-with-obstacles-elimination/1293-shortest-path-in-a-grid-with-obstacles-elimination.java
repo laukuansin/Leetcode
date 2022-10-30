9d627a74d22a2d193129100159c87ac0a13454c7
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-10-30 23:15:53 +0800

    Time: 128 ms (27.50%), Space: 92.1 MB (9.15%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int shortestPath(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int res = 0;
+         boolean[][][] visited = new boolean[m][n][k + 1];
+        Queue<int[]> queue = new LinkedList();
+        queue.add(new int[]{0,0,k});
+        while(queue.size()!=0)
+        {
+            int size = queue.size();
+            for(int i=0;i<size;i++)
+            {
+                int[] temp = queue.poll();
+                int x = temp[0];
+                int y = temp[1];
+                int obs = temp[2];
+                
+                if(x==m-1&&y==n-1&&obs>=0)
+                    return res;
+                
+                if(obs<0||visited[x][y][obs])
+                {
+                    continue;
+                }
+                visited[x][y][obs]=true;
+                if(x>0)
+                {
+                    queue.add(new int[]{x-1,y,obs-grid[x-1][y]});
+                }
+                if(y>0)
+                {
+                    queue.add(new int[]{x,y-1,obs-grid[x][y-1]});
+                }
+                if(x<m-1)
+                {
+                    queue.add(new int[]{x+1,y,obs-grid[x+1][y]});
+                }
+                if(y<n-1)
+                {
+                    queue.add(new int[]{x,y+1,obs-grid[x][y+1]});
+                }
+            }
+            res++;
+        }
+        return -1;
+    }
+    
+   
+}
