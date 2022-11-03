c505ce870889d166358e3b8d9fc10ae461f333d3
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-11-03 23:51:38 +0800

    Time: 412 ms (13.12%), Space: 125.5 MB (22.37%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -2,37 +2,32 @@ import java.util.Hashtable;
 class Solution {
     public int longestPalindrome(String[] words) {
         Hashtable<String,Integer> ht = new Hashtable<>();
-        int palindromeMax = 0;
-        int nonPalindromeMax = 0;
-        boolean oddPalindromeCount = false;
+        int maxlen = 0;
+        int pair = 0;
         for(String word:words)
         {
-            ht.put(word,ht.getOrDefault(word,0)+1);
-        }
-        
-        for(Map.Entry<String,Integer> m:ht.entrySet())
-        {  
-            char[] charArr = m.getKey().toCharArray();
-            if(charArr[0]==charArr[1])
-            {
-                if(m.getValue()%2==0)
-                {
-                    palindromeMax +=m.getValue()*2;
-                }
-                else{
-                    palindromeMax += (m.getValue()-1)*2;
-                    oddPalindromeCount = true;
-                }
-            }
-            else{
-                String reverse = charArr[1]+""+charArr[0];
-                if(ht.containsKey(reverse))
-                {
-                    nonPalindromeMax += Math.min(ht.get(reverse),m.getValue())*2;
-                }
-            }
-        }
+            char[] charArr = word.toCharArray();
+            String rev = charArr[1]+""+charArr[0];
             
-        return oddPalindromeCount?nonPalindromeMax+palindromeMax+2:nonPalindromeMax+palindromeMax;
+            if(ht.containsKey(rev)&&ht.get(rev)>0)
+            {
+                ht.put(rev,ht.getOrDefault(rev,0)-1);
+                maxlen+=4;
+                if(word.equals(rev))
+                    pair--;
+                continue;
+            }
+            
+            if(word.equals(rev))
+                pair++;
+            ht.put(word,ht.getOrDefault(word,0)+1);
+            
+        
+        }
+            if(pair>0)
+            {
+                maxlen+=2;
+            }
+        return maxlen;
     }
 }
