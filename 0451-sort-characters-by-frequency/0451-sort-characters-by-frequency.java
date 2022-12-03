78ba4979987f3fa0af291f020cbed9576147a179
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-12-03 22:00:48 +0800

    Time: 46 ms (39.91%), Space: 55.2 MB (18.77%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,28 +1,32 @@
-
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map.Entry;
 class Solution {
     public String frequencySort(String s) {
         StringBuilder sb = new StringBuilder();
         char[] charArray = s.toCharArray();
         
         HashMap<Character,Integer> hm = new HashMap<>();
+        PriorityQueue<Pair<Character,Integer>> pq = new PriorityQueue<>(new Comparator<Pair<Character,Integer>>(){
+             public int compare(Pair<Character,Integer> p1, Pair<Character,Integer> p2) {
+                return p2.getValue().compareTo(p1.getValue());
+            }
+        });
         
         for(char c: charArray)
         {
             hm.put(c,hm.getOrDefault(c,0)+1);
         }
-        List<Entry<Character, Integer>> nlist = new ArrayList<>(hm.entrySet());
-        nlist.sort(Entry.comparingByValue(Comparator.reverseOrder()));
-        for(Entry<Character,Integer> entry:nlist)           
-       {
-            for(int i=0;i<entry.getValue();i++)
+         for (Map.Entry<Character, Integer> set :hm.entrySet())
+         {
+            pq.add(new Pair<>(set.getKey(),set.getValue()));
+         }
+        
+        while(!pq.isEmpty())
+        {
+            Pair<Character,Integer> pair = pq.remove();
+            for(int i=0;i<pair.getValue();i++)
             {
-                sb.append(entry.getKey());
+                sb.append(pair.getKey());
             }
-       }
+        }
         
         
         return sb.toString();
