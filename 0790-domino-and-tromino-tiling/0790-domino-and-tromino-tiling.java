074700dc4e55a651c0a60325532326b6218ac628
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2022-12-24 21:42:12 +0800

    Time: 0 ms (100.00%), Space: 39.3 MB (82.13%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -0,0 +1,18 @@
+class Solution {
+    int MOD = 1000000007;
+    public int numTilings(int n) {
+        long[] dp = new long[n+1];
+        if(n<2)
+            return n;
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for(int i=3;i<=n;i++)
+        {
+            dp[i] = ((dp[i-1]*2)%MOD+dp[i-3])%MOD;
+        }
+
+        return (int)dp[n]%MOD;
+    }
+}
