9b6ccbf5cfd3171a4027b9a2d76af67efcdfc1e2
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-08-20 22:43:48 +0800

    Time: 4 ms (65.9%), Space: 43.2 MB (98.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,21 @@
-import java.util.Hashtable;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] ans =new int[2];
-        Hashtable<Integer,Integer> ht = new Hashtable<>();
-        for(int i=0;i<nums.length;i++)
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+
+        for(int i = 0 ; i < nums.length ; i++)
         {
-            int key = target-nums[i];
-            if(ht.containsKey(key))
+            hashMap.put(target-nums[i],i);
+        }
+
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            if(hashMap.containsKey(nums[i]))
             {
-                return new int[]{i,ht.get(key)};
-            }
-            else{
-                ht.put(nums[i],i);
+                if(i!=hashMap.get(nums[i]))
+                    return new int[]{i,hashMap.get(nums[i])};
             }
         }
+
         return new int[2];
     }
 }
