029fc125d07fcc45ef44fc8454516ed8119c0840
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-08-25 22:58:34 +0800

    Time: 4 ms (57.53%), Space: 40.8 MB (66.43%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -4,44 +4,26 @@ class Solution {
         {
             return false;
         }
-        if(s1.length()==0)
+        boolean[][] dp = new boolean[s1.length()+1][s2.length()+1];
+        dp[0][0] = true;
+        for(int i = 1 ; i <=s1.length() ; i++)
         {
-            return s2.equals(s3);
+            dp[i][0] = dp[i-1][0]&&s1.charAt(i-1)==s3.charAt(i-1);
         }
-        if(s2.length()==0)
+        for(int i = 1 ; i <=s2.length() ; i++)
         {
-            return s1.equals(s3);
+            dp[0][i] = dp[0][i-1]&&s2.charAt(i-1)==s3.charAt(i-1);
         }
 
-        return helper(s1,s2,s3,0,0,0,new Boolean[s1.length()+1][s2.length()+1]);
+        for(int i = 1 ; i <= s1.length() ; i++)
+        {
+            for(int j = 1; j <= s2.length() ; j++)
+            {
+                dp[i][j] = (dp[i-1][j]&&s1.charAt(i-1)==s3.charAt(i+j-1))||(dp[i][j-1]&&s2.charAt(j-1)==s3.charAt(i+j-1));
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
     }
 
-    public boolean helper(String s1,String s2,String s3, int idx1,int idx2, int idx3,Boolean[][] memo)
-    {
-        if(idx3==s3.length())
-        {
-            return idx1==s1.length()&&idx2==s2.length();
-        }
-
-        if(memo[idx1][idx2]!=null)
-        {
-            return memo[idx1][idx2];
-        }
-
-        boolean selectS1 = false;
-        boolean selectS2 = false;
-        if(idx1<s1.length()&&s1.charAt(idx1)==s3.charAt(idx3))
-        {
-            selectS1 = helper(s1,s2,s3,idx1+1,idx2,idx3+1,memo); 
-        }
-        if(idx2<s2.length()&&s2.charAt(idx2)==s3.charAt(idx3))
-        {
-            selectS2 = helper(s1,s2,s3,idx1,idx2+1,idx3+1,memo);
-        }
-        if(idx1<s1.length()&&idx2<s2.length()&&s1.charAt(idx1)!=s3.charAt(idx3)&&s2.charAt(idx2)!=s3.charAt(idx3))
-        {
-            return false;
-        }
-        return memo[idx1][idx2]=selectS1||selectS2;
-    }
 }
