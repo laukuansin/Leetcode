483b38789def99bcd8a72fab15ed21a3cc0d0c68
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-09-03 10:21:07 +0800

    Time: 0 ms (100%), Space: 39.6 MB (19.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -6,6 +6,7 @@ class Solution {
         {
             dp[i][0] = 1;
         }
+
         for(int i = 0 ; i < n ; i++)
         {
             dp[0][i] = 1;
@@ -13,9 +14,9 @@ class Solution {
 
         for(int i = 1 ; i < m ; i++)
         {
-            for(int j = 1 ; j < n ; j++)
+            for(int j = 1 ; j<n ; j++)
             {
-                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
 
