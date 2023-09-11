af437e8199618f7f38987ff5484f36534ca672b1
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-09-11 21:40:52 +0800

    Time: 7 ms (55.15%), Space: 44.5 MB (59.12%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,39 +1,19 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> ans = new ArrayList<>();
-
         HashMap<Integer,List<Integer>> hashMap = new HashMap<>();
 
         for(int i = 0 ; i < groupSizes.length; i++)
         {
             List<Integer> list = hashMap.getOrDefault(groupSizes[i],new ArrayList<>());
             list.add(i);
+            hashMap.put(groupSizes[i],list);
             if(list.size()==groupSizes[i])
             {
                 ans.add(new ArrayList<>(list));
                 hashMap.remove(groupSizes[i]);
-            }
-            else{
-                hashMap.put(groupSizes[i],list);
-            }
-                
+            }   
         }
-
-        // for (Map.Entry<Integer,List<Integer>> set :hashMap.entrySet()) 
-        // {
-        //     List<Integer> mapList = set.getValue();
-        //     System.out.println(mapList.size());
-        //     List<Integer> list = new ArrayList<>();
-        //     for(int i = 0 ; i < mapList.size() ; i++)
-        //     {
-        //         if(i%set.getKey()==0)
-        //         {
-        //             ans.add(new ArrayList<>(list));
-        //             list.removeAll();
-        //         }
-        //         list.add(mapList.get(i));
-        //     }
-        // }
 
         return ans;
     }
