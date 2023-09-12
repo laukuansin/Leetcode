4c695c2f805678d7a1cf4b55f441600a925ee875
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-09-12 21:46:23 +0800

    Time: 7 ms (98.85%), Space: 44.5 MB (65.1%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minDeletions(String s) {
+        int[] count = new int[26];
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        int ans = 0;
+        for(char c : s.toCharArray())
+        {
+            count[c-'a']++;
+        }
+
+        for(int i = 0 ; i < 26 ; i++)
+        {
+            if(count[i]==0)
+                continue;
+            int frequency = count[i];
+            int curFreq = frequency;
+            while(hashMap.containsKey(curFreq))
+            {
+                curFreq--;
+            }
+
+            if(curFreq>0)
+            {
+                hashMap.put(curFreq,i);
+            }
+            ans+=(frequency-curFreq);
+        }
+
+        return ans;
+    }
+}
