8c84bcbb65efa0903a0915b5512a1a9220246738
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-09-21 09:24:00 +0800

    Time: 5 ms (63.54%), Space: 59.5 MB (47.51%) - LeetHub

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int n = nums.length;
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+        for(int num:nums)
+        {
+            sum+=num;
+        }
+
+        sum-=x;
+        if(sum<0)
+        {
+            return -1;
+        }
+
+        int curSum = 0;
+        int start = 0;
+        int end = 0;
+        //sum = 6
+        
+        while(start<n&&end<n)
+        {
+            curSum+=nums[end];
+            while(start<n&&sum-curSum<0)
+            {
+                curSum-=nums[start++];
+            }
+            if(sum-curSum==0)
+            {
+                ans = Math.max(ans,end-start+1);
+            }
+            end++;
+        }
+
+        return ans==Integer.MIN_VALUE?-1:n-ans;
+    }
+
+}
