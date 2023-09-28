4480d62efe44e227e8bc890702041b36be1f8240
Lau Kuan Sin <49442955+laukuansin@users.noreply.github.com>
2023-09-28 11:21:05 +0800

    Time: 1 ms (81.28%), Space: 43.9 MB (74.25%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,28 +1,56 @@
 class Solution {
-    public int[] sortArrayByParity(int[] nums) {
-        if(nums.length==1)
-        {
-            return nums;
-        }
+    public int[] sortArrayByParity(int[] nums)
+    {
         int start = 0;
+        int end = nums.length - 1;
 
-        while(start < nums.length)
+        while(start < end)
         {
-            int end = start+1;
-            while(end < nums.length)
+            if(nums[start] % 2 == 1 && nums[end] % 2 ==0)
             {
-                if(nums[start]%2 == 1 && nums[end]%2 == 0)
-                {
-                    int tmp = nums[start];
-                    nums[start] = nums[end];
-                    nums[end] = tmp;
-                    break;
-                }
-                end++;
+                int tmp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tmp;
             }
-            start++;
+
+            if(nums[start]%2==0)
+            {
+                start++;
+            }
+
+            if(nums[end]%2==1)
+            {
+                end--;
+            }
         }
 
         return nums;
     }
+
+    //brute force method
+    // public int[] sortArrayByParity(int[] nums) {
+    //     if(nums.length==1)
+    //     {
+    //         return nums;
+    //     }
+    //     int start = 0;
+
+    //     while(start < nums.length)
+    //     {
+    //         int end = start+1;
+    //         while(end < nums.length)
+    //         {
+    //             if(nums[start]%2 == 1 && nums[end]%2 == 0)
+    //             {
+    //                 int tmp = nums[start];
+    //                 nums[start] = nums[end];
+    //                 nums[end] = tmp;
+    //                 break;
+    //             }
+    //             end++;
+    //         }
+    //         start++;
+    //     }
+    //     return nums;
+    // }
 }
